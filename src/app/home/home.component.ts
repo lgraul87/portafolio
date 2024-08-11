6e9f85067bbf1a3fbd9ec6ceef04f11644cf21e4
lgraul87 <lgraul87@gmail.com>
2024-08-11 11:45:49 +0200

    1 more added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,33 +2,32 @@ import { Component } from '@angular/core';
 import { DesktopHeaderComponent } from '../shared/desktop-header/desktop-header.component';
 import { DesktopFooterComponent } from '../shared/desktop-footer/desktop-footer.component';
 import { DesktopContainerComponent } from '../shared/desktop-container/desktop-container.component';
-import {
-  desktopFirstContainer, desktopFirstContainerLeftSide, desktopFirstTape,
-  desktopSecondContainer, desktopThirdContainer, desktopFourthContainer,
-  desktopFifthContainer, desktopSixthContainer, desktopAnimatedAd, desktopFaqs
-} from './home.grid'
-import { CardComponent } from '../shared/card/card.component';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../shared/card/card.component';
+import { ImageComponent } from '../shared/image/image.component';
+import { TitleComponent } from '../shared/title/title.component';
+import { DescriptionComponent } from '../shared/description/description.component';
+import { ButtonComponent } from '../shared/button/button.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, DesktopHeaderComponent, DesktopFooterComponent, DesktopContainerComponent, CardComponent],
+  imports: [
+    CommonModule,
+    DesktopHeaderComponent,
+    DesktopFooterComponent,
+    DesktopContainerComponent,
+    CardComponent,
+    ImageComponent,
+    TitleComponent,
+    DescriptionComponent,
+    ButtonComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
 
-  desktopFirstContainer = desktopFirstContainer;
-  desktopFirstContainerLeftSide = desktopFirstContainerLeftSide;
-  desktopFirstTape = desktopFirstTape;
-  desktopSecondContainer = desktopSecondContainer;
-  desktopThirdContainer = desktopThirdContainer;
-  desktopFourthContainer = desktopFourthContainer;
-  desktopFifthContainer = desktopFifthContainer;
-  desktopSixthContainer = desktopSixthContainer;
-  desktopAnimatedAd = desktopAnimatedAd;
-  desktopFaqs = desktopFaqs;
-
-  cardComponent: any = CardComponent;
 
 }
