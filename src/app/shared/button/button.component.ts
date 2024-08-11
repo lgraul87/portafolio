8b022be863e76027937dc76fd441e09ddac74eab
lgraul87 <lgraul87@gmail.com>
2024-08-11 11:46:17 +0200

    1 more added

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+  @Input()
+  buttonClass: any;
+
+  @Input()
+  text: any;
+}
